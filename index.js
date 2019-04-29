a46b06f4d7f60e370c2818ae4e0a39de20a408f7
darryl dixon <piecedigitalstudios@gmail.com>
2019-04-29 17:47:47 -0400

    all of them

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const cp = require("child_process");
 
 try {
     cp.fork("./dist/app.js", [], {
-        env: {
-            DB_PORT: process.env["DB_PORT"] || 27017,
-            NODE_ENV: process.env["NODE_ENV"] || "dev"
-        }
+        env: Object.assign(process.env, {
+            // DB_PORT: process.env["DB_PORT"] || 27017,
+            // NODE_ENV: process.env["NODE_ENV"] || "dev"
+        })
     });
 } catch (error) {
     console.error(error);
