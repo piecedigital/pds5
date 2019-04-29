67b077250ddf14446e012ffb465259ced440fdcb
darryl dixon <piecedigitalstudios@gmail.com>
2019-04-29 17:47:40 -0400

    get the damn ENVS

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const cp = require("child_process");
 
 try {
     cp.fork("./dist/app.js", [], {
-        env: {}
+        env: {
+            DB_PORT: process.env["DB_PORT"] || 27017,
+            NODE_ENV: process.env["NODE_ENV"] || "dev"
+        }
     });
 } catch (error) {
     console.error(error);
